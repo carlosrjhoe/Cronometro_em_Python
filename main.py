50447c20bb0a72315bc048f6da01cc7c16a07043
Carlos Roberto <carlosrjhoe@gmail.com>
2022-02-12 13:39:04 -0300

    Terminado a emplementação do botão iniciar

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from cProfile import label
+from tempfile import TemporaryDirectory
 from tkinter import *
 import tkinter
 
@@ -25,6 +26,9 @@ janela.resizable(width=False, height=False)
 global tempo
 global rodar
 global contador
+global limitador
+
+limitador = 59
 
 #   Funções do cronômetro
 tempo = '00:00:00'
@@ -35,15 +39,36 @@ contador = -5
 def iniciar():
     global tempo
     global contador
+    global limitador
     if rodar:
         # Antes do cronometro começar, o programa irá começar uma contagem de 5~0 para começar o cronômetro
         if contador <= -1:
             inicio = 'Começando em ' +str(contador)
             label_tempo['text'] = inicio
             label_tempo['font'] = 'Arial 10'
+            
+        #   Rodando o cronômetro
         else:
-            print('Satisfeito')
-        label_tempo.after(1000, iniciar)
+            label_tempo['font'] = 'Times 50 bold'
+            temporario = str(tempo)
+            h, m, s = map(int, temporario.split(':'))
+            h = int(h)
+            m = int(m)
+            s = int(contador)
+            
+            if (s >= limitador):
+                contador = 0
+                m += 1
+            s = str(0) + str(s)
+            m = str(0) + str(m)
+            h = str(0) + str(h)
+            
+            #   Atualizando os valores atuais
+            temporario = str(h[-2:]) + ':' + str(m[-2:]) + ':' + str(s[-2:])
+            label_tempo['text'] = temporario
+            tempo = temporario
+            
+        label_tempo.after(300, iniciar)
         contador += 1
 
 def start():
