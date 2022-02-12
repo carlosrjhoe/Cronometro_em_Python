6dce8d92ad08786ce2d823749377c32542172295
Carlos Roberto <carlosrjhoe@gmail.com>
2022-02-12 13:14:51 -0300

    Modificando texto para comentários

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from tkinter import *
 import tkinter
 
 
-"""Cores:"""
+#   Cores:
 cor1 = "#0a0a0a"  # black
 cor2 = "#fafcff"  # white
 cor3 = "#21c25c"  # green
@@ -12,25 +12,55 @@ cor5 = "#dedcdc"  # gray
 cor6 = "#3080f0"  # blue 
 
 
-'''Criando uma janela'''
+#   Criando uma janela
 janela = Tk()
 janela.title('')
 janela.geometry('300x180')
 janela.configure(bg=cor1)
 
-'''Bloquear o aumento ou diminuição da janela'''
+#   Bloquear o aumento ou diminuição da janela
 janela.resizable(width=False, height=False)
 
-'''Criando labels: Cronômetro'''
-label_app = Label(janela, text='Cronômetro', font=('Arial 10'), bg=cor1, fg=cor2)
-label_tempo = Label(janela, text='00:00:00', font=('Times 50 bold'), bg=cor1, fg=cor6)
+#   Definindo variaveis globais
+global tempo
+global rodar
+global contador
 
-'''Posicionamento para dentro da janela'''
+#   Funções do cronômetro
+tempo = '00:00:00'
+rodar = False
+contador = -5
+
+
+def iniciar():
+    global tempo
+    global contador
+    if rodar:
+        # Antes do cronometro começar, o programa irá começar uma contagem de 5~0 para começar o cronômetro
+        if contador <= -1:
+            inicio = 'Começando em ' +str(contador)
+            label_tempo['text'] = inicio
+            label_tempo['font'] = 'Arial 10'
+        else:
+            print('Satisfeito')
+        label_tempo.after(1000, iniciar)
+        contador += 1
+
+def start():
+    global rodar
+    rodar = True
+    iniciar()
+
+#   Criando labels: Cronômetro
+label_app = Label(janela, text='Cronômetro', font=('Arial 10'), bg=cor1, fg=cor2)
+label_tempo = Label(janela, text=tempo, font=('Times 50 bold'), bg=cor1, fg=cor3)
+
+#   Posicionamento para dentro da janela
 label_app.place(x=20, y=5)
 label_tempo.place(x=20, y=30)
 
-'''Criando botões: Iniciar, Pausa,'''
-botao_iniciar = Button(janela, text='Iniciar', width=10, height=2, bg=cor1, fg=cor2, font=('Ivi 8 bold'), relief='raised', overrelief='ridge')
+#   Criando botões: Iniciar, Pausa, Reiniciar
+botao_iniciar = Button(janela, command=start, text='Iniciar', width=10, height=2, bg=cor1, fg=cor2, font=('Ivi 8 bold'), relief='raised', overrelief='ridge')
 botao_iniciar.place(x=12, y=130)
 
 botao_pausar = Button(janela, text='Pausar', width=10, height=2, bg=cor1, fg=cor2, font=('Ivi 8 bold'), relief='raised', overrelief='ridge')
@@ -38,7 +68,6 @@ botao_pausar.place(x=113, y=130)
 
 botao_reiniciar = Button(janela, text='Reiniciar', width=10, height=2, bg=cor1, fg=cor2, font=('Ivi 8 bold'), relief='raised', overrelief='ridge')
 botao_reiniciar.place(x=211, y=130)
-
 
 
 janela.mainloop()
